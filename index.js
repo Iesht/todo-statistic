7f99ca841f6bd013d3a61a6eea6411e7750511e7
Iesh <108469591+Iesht@users.noreply.github.com>
2025-03-04 17:21:47 +0500

    5, 11

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ function getFiles() {
 function getTODOs() {
   let todos = []
   const exp = /\/\/\s*(TODO|todo|ToDo|tOdO|tODO|toDo|ToDO)\W*[:\s]*(.*)/i;
-  const oldPattern = /^\s*\/\/ TODO\s+(.*)$/;
   for (const file of files) {
     for (const line of file.split('\r\n')) {
       const match = line.match(exp);
@@ -34,47 +33,88 @@ function getTODOs() {
 function parseTODO(todo) {
   let count = todo.split('!').length - 1;
   if (count > 0) {
-    if (!importantTODOs[count]) {
-      importantTODOs[count] = [];
+    if (!importantTODOs[count - 1]) {
+      importantTODOs[count - 1] = [];
     }
-    importantTODOs[count].push(todo);
+    importantTODOs[count - 1].push(todo);
+  } else {
+    let parts = todo.split(';');
+    count = parts.length - 1;
+    if (count === 2) {
+      let name = parts[0].toLowerCase();
+      if (!userTODOs[name]) {
+        userTODOs[name] = [];
+      }
+      userTODOs[name].push(parts[2]);
+    }
   }
 }
 
 function processCommand(command) {
-    switch (true) {
-        case command === 'exit':
-            process.exit(0);
-            break;
-        case command === 'show':
-            for (const todo of TODOs) {
-                console.log(todo);
-            }
-            break;
-        case command === 'important':
-            const sortedKeys = Object.keys(importantTODOs).sort((a, b) => b - a);
+  switch (true) {
+    case command === 'exit':
+      process.exit(0);
+      break;
+    case command === 'show':
+      for (const todo of TODOs) {
+        console.log(todo);
+      }
+      break;
+    case command === 'important':
+      const sortedKeys = Object.keys(importantTODOs).sort((a, b) => b - a);
 
-            for (const key of sortedKeys) {
-                const tasks = importantTODOs[key];
-                for (const task of tasks) {
-                    console.log(task);
-                }
-            }
-            break;
-        case command.startsWith('user'):
-            let user = command.slice(5);
-            if (userTODOs.includes(user)){
-                for (const todo of userTODOs[user]) {
-                    console.log(todo);
-                }
-            } else {
-                console.log(`нет todo с юзером ${user}`);
-            }
-            break;
-        default:
-            console.log('wrong command');
-            break;
-    }
+      for (const key of sortedKeys) {
+        const tasks = importantTODOs[key];
+        for (const task of tasks) {
+          console.log(task);
+        }
+      }
+      break;
+    case command.startsWith('user'):
+      let user = command.slice(5).toLowerCase();
+      if (userTODOs[user] !== []) {
+        for (const todo of userTODOs[user]) {
+          console.log(todo);
+        }
+      } else {
+        console.log(`нет todo с юзером ${user}`);
+      }
+      break;
+    case command.startsWith('sort'):
+      let sortType = command.slice(5).trim();
+      switch (true) {
+        case sortType === 'importance':
+          TODOs.sort((a, b) => (b.match(/!/g) || []).length - (a.match(/!/g) || []).length);
+          TODOs.forEach(todo => console.log(todo));
+          break;
+        case sortType === 'user':
+          Object.keys(userTODOs)
+            .sort((a, b) => (a === '' ? 1 : b === '' ? -1 : a.localeCompare(b)))
+            .forEach(user => {
+              userTODOs[user].forEach(todo => console.log(`${user}: ${todo}`))
+            })
+          const anonTODOs = TODOs.filter(todo => !todo.includes(';'));
+          if (anonTODOs.length > 0) {
+            console.log('--- Без пользователя ---');
+            anonTODOs.forEach(todo => console.log(todo));
+          }
+          break;
+        case sortType === 'date':
+          TODOs.sort((a, b) => {
+            let dateA = a.match(/\d{4}-\d{2}-\d{2}/);
+            let dateB = b.match(/\d{4}-\d{2}-\d{2}/);
+            if (!dateA) return 1;
+            if (!dateB) return -1;
+            return new Date(dateB[0]) - new Date(dateA[0]);
+          });
+          TODOs.forEach(todo => console.log(todo));
+          break;
+      }
+      break;
+    default:
+      console.log('wrong command');
+      break;
+  }
 }
 
 // TODO you can do it!
