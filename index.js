1ea989dfb9a1fc8065d75e4a2209c3b89033f108
volodout <volodout@gmail.com>
2025-03-04 17:08:13 +0500

    6

--- index.js
+++ index.js
@@ -10,35 +10,35 @@ console.log('Please, write your command!');
 readLine(processCommand);
 
 function getFiles() {
-  const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-  return filePaths.map(path => readFile(path));
+    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
+    return filePaths.map(path => readFile(path));
 }
 
 function getTODOs() {
-  let todos = []
-  const exp = /\/\/\s*(TODO|todo|ToDo|tOdO|tODO|toDo|ToDO)\W*[:\s]*(.*)/i;
-  const oldPattern = /^\s*\/\/ TODO\s+(.*)$/;
-  for (const file of files) {
-    for (const line of file.split('\r\n')) {
-      const match = line.match(exp);
-      if (match) {
-        let todo = match[2];
-        todos.push(todo);
-        parseTODO(todo);
-      }
+    let todos = []
+    const exp = /\/\/\s*(TODO|todo|ToDo|tOdO|tODO|toDo|ToDO)\W*[:\s]*(.*)/i;
+    const oldPattern = /^\s*\/\/ TODO\s+(.*)$/;
+    for (const file of files) {
+        for (const line of file.split('\r\n')) {
+            const match = line.match(exp);
+            if (match) {
+                let todo = match[2];
+                todos.push(todo);
+                parseTODO(todo);
+            }
+        }
     }
-  }
-  return todos;
+    return todos;
 }
 
 function parseTODO(todo) {
-  let count = todo.split('!').length - 1;
-  if (count > 0) {
-    if (!importantTODOs[count]) {
-      importantTODOs[count] = [];
+    let count = todo.split('!').length - 1;
+    if (count > 0) {
+        if (!importantTODOs[count]) {
+            importantTODOs[count] = [];
+        }
+        importantTODOs[count].push(todo);
     }
-    importantTODOs[count].push(todo);
-  }
 }
 
 function processCommand(command) {
@@ -61,14 +61,41 @@ function processCommand(command) {
                 }
             }
             break;
+
         case command.startsWith('user'):
             let user = command.slice(5);
-            if (userTODOs.includes(user)){
+            if (userTODOs.includes(user)) {
                 for (const todo of userTODOs[user]) {
                     console.log(todo);
                 }
             } else {
                 console.log(`нет todo с юзером ${user}`);
+            }
+            break;
+
+        case command.startsWith('date '):
+            const dateStr = command.slice(5).trim();
+            if (!/^\d{4}(-\d{2})?(-\d{2})?$/.test(dateStr)) {
+                console.log('wrong date format');
+                break;
+            }
+
+            const parts = dateStr.split('-');
+            const year = parts[0];
+            const month = parts[1] || '01';
+            const day = parts[2] || '01';
+            const inputDate = new Date(year, month - 1, day);
+
+            const filteredTODOs = TODOs.filter(todo => {
+                const todoParts = todo.split(';');
+                if (todoParts.length < 2) return false;
+                const todoDateStr = todoParts[1].trim();
+                const todoDate = new Date(todoDateStr);
+                if (isNaN(todoDate)) return false;
+                return todoDate > inputDate;
+            });
+            for (const t of filteredTODOs) {
+                console.log(t);
             }
             break;
         default:
