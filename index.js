a1d5b6a28a9d38de4462b290c00ca6091f260125
volodout <volodout@gmail.com>
2025-03-04 16:11:29 +0500

    3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const TODOs = [];
+const importantTODOs = {}
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -15,6 +17,21 @@ function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (const todo of TODOs) {
+                console.log(todo);
+            }
+            break;
+        case 'important':
+            const sortedKeys = Object.keys(importantTODOs).sort((a, b) => b - a);
+
+            for (const key of sortedKeys) {
+                const tasks = importantTODOs[key];
+                for (const task of tasks) {
+                    console.log(task);
+                }
+            }
             break;
         default:
             console.log('wrong command');
