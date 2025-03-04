250fb932112905fd366a9c8928196582a1dee957
Iesh <108469591+Iesht@users.noreply.github.com>
2025-03-04 15:53:49 +0500

    1, 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const TODOs = getTODOs();
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -11,10 +12,28 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getTODOs() {
+    let todos = []
+    for (const file of files) {
+        for (const line of file.split('\r\n')) {
+            const match = line.match(/^\/\/ TODO\s+(.*)$/);
+            if (match) {
+                todos.push(match[1]);
+            }
+        }
+    }
+    return todos;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (const todo of TODOs) {
+                console.log(todo);
+            }
             break;
         default:
             console.log('wrong command');
