606cf0c5aa7a1ff70765516c3fa9436955301c91
Iesh <108469591+Iesht@users.noreply.github.com>
2025-03-04 16:34:02 +0500

    3 + fixes

--- index.js
+++ index.js
@@ -2,43 +2,60 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const importantTODOs = {};
+const userTODOs = {};
 const TODOs = getTODOs();
 
 console.log('Please, write your command!');
 readLine(processCommand);
 
 function getFiles() {
-    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+  const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
+  return filePaths.map(path => readFile(path));
 }
 
 function getTODOs() {
-    let todos = []
-    for (const file of files) {
-        for (const line of file.split('\r\n')) {
-            const match = line.match(/^\/\/ TODO\s+(.*)$/);
-            if (match) {
-                todos.push(match[1]);
-            }
-        }
+  let todos = []
+  const exp = /\/\/\s*(TODO|todo|ToDo|tOdO|tODO|toDo|ToDO)\W*[:\s]*(.*)/i;
+  const oldPattern = /^\s*\/\/ TODO\s+(.*)$/;
+  for (const file of files) {
+    for (const line of file.split('\r\n')) {
+      const match = line.match(exp);
+      if (match) {
+        let todo = match[2];
+        todos.push(todo);
+        parseTODO(todo);
+      }
     }
-    return todos;
+  }
+  return todos;
+}
+
+function parseTODO(todo) {
+  let count = todo.split('!').length - 1;
+  if (count > 0) {
+    if (!importantTODOs[count]) {
+      importantTODOs[count] = [];
+    }
+    importantTODOs[count].push(todo);
+  }
 }
 
 function processCommand(command) {
-    switch (command) {
-        case 'exit':
-            process.exit(0);
-            break;
-        case 'show':
-            for (const todo of TODOs) {
-                console.log(todo);
-            }
-            break;
-        default:
-            console.log('wrong command');
-            break;
-    }
+  switch (command) {
+    case 'exit':
+      process.exit(0);
+      break;
+    case 'show':
+      for (const todo of TODOs) {
+        console.log(todo);
+      }
+      console.log(importantTODOs);
+      break;
+    default:
+      console.log('wrong command');
+      break;
+  }
 }
 
 // TODO you can do it!
